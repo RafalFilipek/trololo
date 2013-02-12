56fad0e9a726218c5ec9e79011a41ddc5b26839f
Rafał Filipek <rafal.filipek@gmail.com>
2013-02-12 22:53:50 +0100

    remove, lowercase

--- app.py
+++ app.py
@@ -3,15 +3,13 @@
 from flask import Flask
 from flask import render_template
 
-from json import dumps
-
-from api import API
+from api import api
 
 app = Flask(__name__)
 
 app.debug = True
 
-app.register_blueprint(API)
+app.register_blueprint(api)
 
 @app.route('/')
 def index():
